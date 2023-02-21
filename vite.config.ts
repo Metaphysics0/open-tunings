470338f7d15a27032bfb1d2d60611de89f4ac846
Ryan Roberts <ryanr@cityhive.net>
2023-02-21 22:27:22 +0200

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+import UnoCss from 'unocss/vite';
+import { extractorSvelte, presetUno, presetWebFonts } from 'unocss';
+
+export default defineConfig({
+	plugins: [
+		UnoCss({
+			extractors: [extractorSvelte],
+			shortcuts: [
+				{
+					logo: 'i-logos:svelte-icon w-6em h-6em transform transition-800 hover:rotate-180'
+				}
+			],
+			presets: [
+				presetUno(),
+				presetWebFonts({
+					provider: 'google',
+					fonts: {
+						sans: 'Nunito'
+					}
+				})
+			]
+		}),
+		sveltekit()
+	]
+});
