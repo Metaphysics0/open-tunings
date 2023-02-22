f63015815352dca6efa9cf6ff46b2df8f2eb1591
Ryan Roberts <ryanr@cityhive.net>
2023-02-22 19:45:12 +0200

    switch to npm, add note items

---
+++ src/constants/note.ts
@@ -0,0 +1,3 @@
+export const MUSICAL_NOTES = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"];
+export const musicalNotesWithFlatPreference = ["A", "Bb", "B", "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab"];
+
