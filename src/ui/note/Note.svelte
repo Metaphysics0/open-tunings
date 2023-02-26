cf0611ce27a1611a8f0261cb31532287f6b18d58
Ryan Roberts <ryanr@cityhive.net>
2023-02-26 22:19:28 +0200

    better ui

--- src/ui/note/Note.svelte
+++ src/ui/note/Note.svelte
@@ -1,21 +1,43 @@
 <script lang="ts">
   import { notePlayer } from '../../services/NotePlayer';
   import type { INoteItem } from '../../types/note';
+  import PitchShiftButton from './PitchShiftButton.svelte';
 
   export let noteItem: INoteItem;
+  let shouldShowOctave: boolean = false;
 
   function playSingleNote(): void {
     notePlayer.play(noteItem);
   }
 </script>
 
-<div>
+<div
+  class="flex flex-col items-center justify-center mx-3"
+  on:mouseover={() => (shouldShowOctave = true)}
+  on:focus={() => (shouldShowOctave = true)}
+  on:mouseleave={() => (shouldShowOctave = false)}
+>
+  <PitchShiftButton direction="up" />
   <button
-    class="bg-slate-2 rounded-xl p-3 w-fit mx-2 flex flex-col cursor-pointer"
+    class="bg-slate-2 rounded-xl p-5 w-fit flex flex-col cursor-pointer"
     on:click={playSingleNote}
   >
-    <p class="text-xl">
+    <p class="relative text-3xl font-extrabold">
       {noteItem.note}
+      {#if shouldShowOctave}
+        <span class="absolute block opacity-30">
+          {noteItem.octave}
+        </span>
+      {/if}
     </p>
   </button>
+  <PitchShiftButton direction="down" />
 </div>
+
+<style>
+  span {
+    font-size: 50%;
+    top: 10px;
+    right: -10px;
+  }
+</style>
---
+++ src/ui/note/PitchShiftButton.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import FaAngleUp from 'svelte-icons/fa/FaAngleUp.svelte';
+  import FaAngleDown from 'svelte-icons/fa/FaAngleDown.svelte';
+  export let direction: 'up' | 'down';
+
+  function onClick(): void {
+    console.log('I WAS CLICKED');
+  }
+</script>
+
+<button on:click={onClick} class="w-8 h-7 opacity-40">
+  {#if direction === 'up'}
+    <FaAngleUp />
+  {:else}
+    <FaAngleDown />
+  {/if}
+</button>
