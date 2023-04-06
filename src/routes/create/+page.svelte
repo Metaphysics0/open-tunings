799a0a004cad03bfe4b36ef1779221d761939de4
Ryan Roberts <ryanr@cityhive.net>
2023-04-06 14:06:55 +0300

    created create page

---
+++ src/routes/create/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import type { UserSubmittedTuning } from '@prisma/client';
+
+  import CurrentTuningName from '../../ui/CurrentTuningName.svelte';
+  import { currentTuning } from '../../stores';
+  import Note from '../../ui/note/Note.svelte';
+
+  let currentTuningNotes: UserSubmittedTuning['tuning'];
+  currentTuning.subscribe((value) => {
+    currentTuningNotes = value;
+  });
+</script>
+
+<CurrentTuningName />
+<section class="flex mb-3">
+  {#each currentTuningNotes as noteItem, index}
+    <Note {noteItem} {index} />
+  {/each}
+</section>
--- src/routes/tuning/[[friendlyName]]/+page.server.ts
+++
@@ -1,7 +0,0 @@
-export const load = ({ params }) => {
-  console.log('PARAMSA', params);
-
-  return {
-    name: 'ryan'
-  };
-};
